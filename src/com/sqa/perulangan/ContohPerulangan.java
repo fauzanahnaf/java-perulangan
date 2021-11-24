92529841bc2b35d778d5799062b0b5967c9026c5
fauzanahnaf <fauzanahnaf23@gmail.com>
2021-11-24 10:25:16 +0700

    First Commit

---
+++ src/com/sqa/perulangan/ContohPerulangan.java
@@ -0,0 +1,10 @@
+package com.sqa.perulangan;
+
+public class ContohPerulangan {
+	public static void main(String[] args) {
+		int n = 8;
+		for(int i=1; i<=11; i++) {
+			System.out.printf("%d x %d = %d%n", n, i, n*i);
+		}
+	}
+}
